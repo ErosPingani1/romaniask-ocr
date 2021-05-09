c518acc085fd2096df5175b39cf3da8553b67268
ErosPingani1 <e.pingani3@gmail.com>
2021-05-09 08:18:38 +0200

    Created Flask service with Heroku Procfile

---
+++ application.py
@@ -0,0 +1,17 @@
+# Service main file
+from flask import Flask, request
+from service import image_ocr, delete_image
+
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    return 'Server is running'
+
+@app.route('/imageocr', methods=['POST'])
+def recognize_image():
+    imageB64 = request.get_json()['image']
+    ocr = image_ocr(imageB64)
+    delete_image()
+    return ocr
+    
