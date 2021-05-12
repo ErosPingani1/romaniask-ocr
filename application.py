0a340d5d30b57be767ba8f5a7332012379b3be6e
ErosPingani1 <e.pingani3@gmail.com>
2021-05-12 07:08:08 +0200

    [DEV][BE] - Added CORS management

--- application.py
+++ application.py
@@ -1,8 +1,10 @@
 # Service main file
 from flask import Flask, request
+from flask_cors import CORS
 from service import image_ocr, delete_image
 
-app = Flask(__name__)
+app = Flask(__name__) 
+CORS(app)
 
 @app.route('/')
 def homepage():
